d61d8b5eafa64e4cc962cb48a76bc202c14058d6
YoucefMac <youcefbou1998@gmail.com>
2023-03-27 11:26:50 +0100

    add colors

---
+++ ViberApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViberApp.swift
+//  Viber
+//
+//  Created by apple on 27/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct ViberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ View/Screens/WelcomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeScreen.swift
+//  Viber
+//
+//  Created by apple on 27/3/2023.
+//
+
+import SwiftUI
+
+struct WelcomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeScreen()
+    }
+}
